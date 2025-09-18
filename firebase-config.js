f77a974dc5639b15a44ed91d8c0d82d6521f20df
Aren Köprülü <arenkoprulu@gmail.com>
2025-09-18 20:42:47 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDM-RNlgi1n1E40rTl7jA6ge5m87PVHX2Y",
+    authDomain: "endurance-vi.firebaseapp.com",
+    projectId: "endurance-vi",
+    storageBucket: "endurance-vi.firebasestorage.app",
+    messagingSenderId: "106145189539",
+    appId: "1:106145189539:web:4ede195cafe764936a3916"
+};
